0caa935773f837b62f1b8d4a1433a5e8bbe05db1
Christian Koller <christian.koller@innoq.com>
2018-04-19 15:00:04 +0200

    nextBlock function renamed to nextBlockCandidate and use hashBlock function

--- model/chain.js
+++ model/chain.js
@@ -1,17 +1,18 @@
-const Block = require("./block").Block
+const Block = require("./block").Block;
+const hashBlock = require("./helper").hashBlock;
 
 const chain  = [];
 
 const b = new Block("0", 1);
 b.timestamp = 0;
-b.proof = 1917336
+b.proof = 1917336;
 chain.push(b);
 
 exports.chain = chain;
 
-exports.nextBlock = function() {
+exports.nextBlockCandidate = function() {
     return new Block(
-        chain[chain.length -1],
+        hashBlock(chain[chain.length -1]),
         chain.length
-    )
+    );
 }
