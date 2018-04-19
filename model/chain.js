98cf70230dd96c4bd4c0f299f2d0e45b2f299af0
Christian Koller <christian.koller@innoq.com>
2018-04-19 16:30:52 +0200

    extracted methods createChain and createGenesisBlock

--- model/chain.js
+++ model/chain.js
@@ -1,13 +1,19 @@
 const Block = require("./block").Block;
 const hashBlock = require("../helper").hashBlock;
 
-const chain  = [];
+const chain  = createChain();
 
-const b = new Block("0", 1);
-b.timestamp = 0;
-b.proof = 1917336;
-b.transactions.push({"id":"b3c973e2-db05-4eb5-9668-3e81c7389a6d","timestamp":0,"payload":"I am Heribert Innoq"});
-chain.push(b);
+function createChain() {
+    return [createGenesisBlock()];
+}
+
+function createGenesisBlock() {
+    const b = new Block("0", 1);
+    b.timestamp = 0;
+    b.proof = 1917336;
+    b.transactions.push({"id":"b3c973e2-db05-4eb5-9668-3e81c7389a6d","timestamp":0,"payload":"I am Heribert Innoq"});
+    return b;
+}
 
 exports.chain = chain;
 
