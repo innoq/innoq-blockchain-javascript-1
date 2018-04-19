6e81a3bcb2f78c866dbcff3808825ac4d947cc58
Goran Vukovic <goran.vukovic@innoq.com>
2018-04-19 14:58:31 +0200

    information service now returns information about our chain instead of a default text

--- services/Information.js
+++ services/Information.js
@@ -1,5 +1,8 @@
+const { chain } = require("../model/chain");
+const uuidv1 = require('uuid/v1');
+
 const getInfo = function(req, res) {
-	res.send("Information Service");
+	res.send(JSON.stringify({"nodeId": uuidv1(), "currentBlockHeight": chain.length}));
 }
 
 exports.getInfo = getInfo;
