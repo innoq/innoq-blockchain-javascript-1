3c5e01b6008f11dd9b1b3f6c2de061affae2de0c
Christian Koller <christian.koller@innoq.com>
2018-04-20 10:00:29 +0200

    added neighbours

---
+++ model/neighbours.js
@@ -0,0 +1,14 @@
+const neighbours  = [];
+
+function addNeighbour(node) {
+
+}
+
+class Node {
+    constructor(nodeId, host) {
+        this.nodeId = nodeId;
+        this.host = host;
+    }
+}
+
+exports.nodeList = nodeList;
