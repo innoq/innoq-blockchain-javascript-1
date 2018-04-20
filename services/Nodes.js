253677b20b6ce6917d047689646fdc83bbab3e77
Christian Koller <christian.koller@innoq.com>
2018-04-20 12:31:41 +0200

    check request parameters nodeId and host

--- services/Nodes.js
+++ services/Nodes.js
@@ -2,8 +2,14 @@ const bodyParser = require("body-parser");
 const addNeighbour = require("../model/neighbours").addNeighbour
 
 const register = function(req, res) {
-    const node = addNeighbour(req.body.nodeId, req.body.host);
-    res.send({"message": "New node added", node});
+    const nodeId = req.body.nodeId;
+    const host = req.body.host;
+    if (nodeId != null && host != null) {
+        const node = addNeighbour(nodeId, host);
+        res.send({"message": "New node added", node});
+    } else {
+        res.status(400).send();
+    }
 }
 
 exports.register = register;
